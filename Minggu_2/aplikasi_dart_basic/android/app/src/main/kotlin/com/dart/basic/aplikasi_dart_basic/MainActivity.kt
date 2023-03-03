7df2463ddbd302ed1b198d95ec8fb3bb1007c1d7
ArZHa03 <arianto.zaki.hamdalah.03@gmail.com>
2023-03-03 20:45:29 +0700

    bilangan prima

---
+++ Minggu_2/aplikasi_dart_basic/android/app/src/main/kotlin/com/dart/basic/aplikasi_dart_basic/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dart.basic.aplikasi_dart_basic
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Minggu_2/aplikasi_dart_basic/lib/main.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 1;
+  String _text = "";
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+      if(_counter>10) {
+        _counter = 1;
+        _text = "Bilangan Prima: ";
+      }
+
+      // check if prime
+      int isPrime = 1;
+      for(int i=2; i<_counter; i++) {
+        if(_counter%i == 0) {
+          isPrime = 0;
+          break;
+        }
+      }
+
+      // add to text
+      for(int i=2; i<=_counter; i++) {
+        if(isPrime == 1 && i == _counter) {
+          _text += '$i, ';
+        }
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            Text(
+              _text,
+              style: Theme.of(context).textTheme.headlineMedium,
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
